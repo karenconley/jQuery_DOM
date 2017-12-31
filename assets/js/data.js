4cf22106192a872101ba64c32bced62731c647e6
nerdale <alexandra.neira@gmail.com>
2017-12-31 16:42:03 -0300

    first commit

---
+++ assets/js/data.js
@@ -0,0 +1,65 @@
+var data = [
+    {
+        "nombre": "Daniela Mora",
+        "edad": "26 años",
+        "foto": "img/dana.jpg",
+        "nacionalidad": "Chilena",
+        "sede": "Chile"
+    },
+    {
+        "nombre": "Karen Orozco",
+        "edad": "27 años",
+        "foto": "img/karen.jpg",
+        "nacionalidad": "Mexicana",
+        "sede": "México"
+    },
+    {
+        "nombre": "Diana Navarro",
+        "edad": "19 años",
+        "foto": "img/diana.jpg",
+        "nacionalidad": "Peruana",
+        "sede": "Perú"
+    },
+    {
+        "nombre": "Paola Ortiz",
+        "edad": "25 años",
+        "foto": "img/paola.jpeg",
+        "nacionalidad": "Peruana",
+        "sede": "Perú"
+    },
+    {
+        "nombre": "Maryori Quiroz",
+        "edad": "19 años",
+        "foto": "img/maryori.jpg",
+        "nacionalidad": "Peruana",
+        "sede": "Perú"
+    },
+    {
+        "nombre": "Susana Opazo",
+        "edad": "25 años",
+        "foto": "img/su.jpg",
+        "nacionalidad": "Chilena",
+        "sede": "Chile"
+    },
+    {
+        "nombre": "Alexandra Neira",
+        "edad": "30 años",
+        "foto": "img/alexandra.jpg",
+        "nacionalidad": "Chilena",
+        "sede": "Chile"
+    },
+    {
+        "nombre": "Elisa Martinez",
+        "edad": "25 años",
+        "foto": "img/elisa.jpg",
+        "nacionalidad": "Mexicana",
+        "sede": "México"
+    },
+    {
+        "nombre": "Maciel Porraz",
+        "edad": "27 años",
+        "foto": "img/maciel.jpg",
+        "nacionalidad": "Mexicana",
+        "sede": "México"
+    }
+]
